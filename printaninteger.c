5f596dada8c8e73b8084407fd6967a22c66afb89
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-08 22:44:15 -0800

    Add files via upload

---
+++ printaninteger.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    printf("number=%d",num);
+
+    return 0;
+}
