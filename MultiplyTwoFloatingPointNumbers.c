a7ec725ad8ab1cb5b508bf986e313be1b3b81098
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-09 11:02:39 -0800

    Add files via upload

---
+++ MultiplyTwoFloatingPointNumbers.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float num1;
+float num2;
+float multiply;
+scanf("%f%f",&num1,&num2);
+multiply=num1*num2;
+printf("%f",multiply);
+
+    return 0;
+}
