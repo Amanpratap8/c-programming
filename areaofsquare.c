174892fdd052f0d7285e68bf071f52da27e98309
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-12 00:21:44 +0530

    Add files via upload

---
+++ areaofsquare.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+   int side,area ;
+   scanf("%d",&side);
+   area=side*side;
+   printf("Aera of square :%d",area);
+    
+
+    return 0;
+}
