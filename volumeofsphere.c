e904bc08e27b5c039926bba434506f32eb02e6e1
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-12 00:41:07 +0530

    Add files via upload

---
+++ volumeofsphere.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+  float r,volume ;
+   scanf("%f",&r);
+   volume=1.3*3.14*r*r*r;
+   printf("Volume of sphere :%F",volume);
+    
+
+    return 0;
+}
