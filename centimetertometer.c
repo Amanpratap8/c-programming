21b9cca1a44c0b780aa471adf6a45fdf9cbc9768
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-12 01:16:03 +0530

    Add files via upload

---
+++ centimetertometer.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+  int centimeter,meter,kilometer;
+  
+  scanf("%d",&centimeter);
+  meter=centimeter*100;
+   kilometer=centimeter*1000;
+   printf("meter:%d",meter);
+   printf("\nkilometer:%d",kilometer);
+    
+
+    return 0;
+}
