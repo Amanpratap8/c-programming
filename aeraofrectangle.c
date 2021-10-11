a2af39965ef80212b8deb12210f13a54224d5e1c
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-12 00:54:08 +0530

    Add files via upload

---
+++ aeraofrectangle.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+  int l,b,area ;
+   scanf("%d%d",&l,&b);
+   area= (l*b);
+   printf("Aera of rectangle:%d",area);
+    
+
+    return 0;
+}
