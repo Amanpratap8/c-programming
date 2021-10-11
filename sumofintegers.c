fe7464adcf612dadf5e2c3658d14f62e2f7dcd89
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-12 01:00:49 +0530

    Add files via upload

---
+++ sumofintegers.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+  int x,y,sum ;
+  x=2;
+  y=4;
+   sum= x+y;
+   printf("Sum of integers:%d",sum);
+    
+
+    return 0;
+}
