2d1b5c19876ffd4312afa2ef8c63cfe06129447f
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-12 00:31:33 +0530

    Add files via upload

---
+++ aeraoftrapezium.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+  float a,b,h,area ;
+   scanf("%f%f%f",&a,&b,&h);
+   area=((a+b)/2)*h;
+   printf("Aera of Trapezium :%F",area);
+    
+
+    return 0;
+}
