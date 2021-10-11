ba41b9a5a33accc9df1a2b00e1217981d6be2bcb
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-12 00:51:03 +0530

    Add files via upload

---
+++ perimeter.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+  int l,b,perimeter ;
+   scanf("%d%d",&l,&b);
+   perimeter= 2*(l+b);
+   printf("Perimeter of rectangle:%d",perimeter);
+    
+
+    return 0;
+}
