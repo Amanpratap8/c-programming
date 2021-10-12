e86e160ae8e5f02d8461df03245d6f3fe0acfb32
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-12 22:12:16 +0530

    Add files via upload

---
+++ aeraofequilateraltri.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a,area;
+    scanf("%f",&a);
+    area= 0.433*a*a;
+    printf("Area of Equilateral triangle :%f",area);
+    return 0;
+}
