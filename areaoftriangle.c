d928c8e0c5b3a0a7f0feb0dafc9120cba29db6c0
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-12 21:50:05 +0530

    Add files via upload

---
+++ areaoftriangle.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float b,h,area;
+    scanf("%f%f",&b,&h);
+    area= 0.5*b*h;
+    printf("Area of triangle :%f",area);
+    return 0;
+}
