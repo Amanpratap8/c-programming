1efdb36f7310bfba5c4211f329f5a118b475d590
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-12 21:31:55 +0530

    Add files via upload

---
+++ squareroot.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float x,y,z;
+    scanf("%f",&x);
+    y=0.5;
+    z= pow(x,y);
+    printf("z:%f",z);
+    return 0;
+}
