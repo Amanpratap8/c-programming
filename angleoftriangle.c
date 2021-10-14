6d8bb34cd264897fd4a8b5d241cf283cf18b74e2
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-14 10:17:19 +0530

    Add files via upload

---
+++ angleoftriangle.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int x,y,z;
+    scanf("%d%d",&x,&y);
+    z=180-(x+y);
+    printf("The value of z:%d",z);
+
+    return 0;
+}
