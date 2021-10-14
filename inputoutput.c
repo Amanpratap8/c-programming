0aab79d7f4ea404545ccfaa60fa679e5190e5cb3
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-14 12:01:40 +0530

    Add files via upload

---
+++ inputoutput.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("a=%d \nb=%d c=%d",a,b,c);
+    
+    
+    
+    
+    return 0;
+}
