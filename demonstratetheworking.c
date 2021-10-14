2280969e75bab463829bb508298bb36aa9fab11c
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-14 11:44:15 +0530

    Add files via upload

---
+++ demonstratetheworking.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    long b;
+    long long c;
+    double e;
+    long double f;
+    printf("size of int=%id bytes\n",sizeof(a));
+    printf("size of long=%id bytes\n",sizeof (b));
+    printf("size of long long=%id bytes\n",sizeof (c));
+    printf("size of double=%id bytes\n",sizeof (e));
+    printf("size of long double=%id bytes\n",sizeof (f));
+        
+    
+    return 0;
+}
