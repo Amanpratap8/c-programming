f77574f5e7ecbd039844e9ac7e85f205010ec33d
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-14 11:44:37 +0530

    Add files via upload

---
+++ swapinftwonumber.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+     a=10;
+     b=20;
+     a=a+b;
+     b=a-b;
+     a=a-b;
+    printf("a=%d \nb=%d",a,b);
+    
+    
+    
+    
+    return 0;
+}
