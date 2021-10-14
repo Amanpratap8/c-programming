8d138e2d456a0aadc93c0e66c94abd1125f3ad15
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-14 10:42:50 +0530

    Add files via upload

---
+++ sizeof.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    float per;
+    double rs;
+    char gender;
+    age=18;
+    per=83.44;
+    rs=70000;
+    gender='M';
+    printf("\nsize of int;%d",sizeof(int));
+    printf("\nsize of float:%d",sizeof(float));
+    printf("\nsize of double:%d",sizeof(double));
+    printf("\nsize of char:%d",sizeof(char));
+    return 0;
+}
