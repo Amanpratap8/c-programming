6877427c4545c05db9395ad87caabde8e64b99f7
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-15 19:41:32 +0530

    Add files via upload

---
+++ escapecharcter.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{ 
+   printf("Aman\bpratap\n");
+   printf("Aman \npratap\n");
+   printf("aman \tpratap\n");
+   printf("aman \vpratap\n");
+   printf("aman \\pratap");
+    
+    
+    return 0;
+}
+
+
+
