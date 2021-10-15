c12edbd4d4ca691d6e6d9d92d705e6d7696a6c5a
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-15 19:41:54 +0530

    Add files via upload

---
+++ mobileno.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <conio.h>
+int main()
+{ 
+   long int a ;
+scanf("%ld",&a);
+printf("%ld",a); 
+    
+    return 0;
+}
+
+
+
