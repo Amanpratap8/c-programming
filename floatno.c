025c3241363c3e6e6bbc3d7f83161df31c5c1c4b
Amanpratap8 <92160707+Amanpratap8@users.noreply.github.com>
2021-10-15 19:42:25 +0530

    Add files via upload

---
+++ floatno.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main( )
+{
+
+  float num1,num2,sum;
+ 
+  printf("Enter first number: ");
+  scanf("%f",&num1); 
+  printf("Enter second number: ");
+  scanf("%f",&num2);
+  sum=num1+num2;
+  printf("Sum of two numbers = %f",sum);
+return 0 ;
+}
+
+
